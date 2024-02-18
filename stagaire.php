c80cce1bbcba74123525b26337d99b092b56795b
Hanane_ELHADDAR <hananelhaddar51@gmail.com>
2024-02-18 19:53:58 +0100

    l'ajoute d'une page de options

--- stagaire.php
+++ stagaire.php
@@ -7,10 +7,98 @@
     <link rel="stylesheet" href="stagaire.css">
 </head>
 <body>
-    <h1>Quiz</h1>
+    <div class="about-section">
+        <h1>Quiz</h1>
+        <!-- <img src="logo2.avif" alt="" style="width:10%" height=":10%"> -->
+</div>
+<div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="HTML5_logo_and_wordmark.svg.png" alt="" style="width:50%" height=":50%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="CSS3_logo_and_wordmark.svg.png" alt="" style="width:37%" height=":37%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="image.webp" alt="" style="width:46%" height=":46%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+      <br>
+    </div>
+  </div>
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="phpp-logo.png" alt="" style="width:50%" height=":50%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="python.png" alt="" style="width:45%" height=":45%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+  <div class="column">
+    <div class="card">
+     <img src="react.png" alt="" style="width:50%" height=":50%" class="img">
+      <div class="container">
+        <p class="title"> basic c language questions </p>
+        <a href="home.php"> <button class="button" > start</p></button></a>
+      </div>
+    </div>
+  </div>
     
 
-<div class="container">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="container">
     <div class="image">
         <img src="HTML5_logo_and_wordmark.svg.png" alt="">
         <p>html</p>
@@ -30,7 +118,7 @@
     <div class="image">
         <img src="python.png" alt="">
         <p>Python</p>
-    </div>
+    </div> -->
 
 </div>
 
