34a9c70ed2f31e5e3c7239a91a0b894f53b6da6c
Hanane_ELHADDAR <hananelhaddar51@gmail.com>
2024-02-24 21:47:29 +0100

    gerer les options

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -17,7 +17,7 @@ margin-left: 20px;
 
 
 
-
+/* 
 </style>
 </head>
 <body>
@@ -58,41 +58,41 @@ margin-left: 20px;
 
 // ?>
 
-<?php
-require "conexion.php";
+// <?php
+// require "conexion.php";
 
-$id_question = 3; // Remplacez par l'id de la question que vous souhaitez récupérer
+// $id_question = 3; // Remplacez par l'id de la question que vous souhaitez récupérer
 
-// Sélectionnez le titre de la question et les options associées
-$req = "SELECT questions.titre_question, options.option_text 
-        FROM questions 
-        LEFT JOIN options ON questions.id_question = options.id_question
-        WHERE questions.id_question = $id_question";
+// // Sélectionnez le titre de la question et les options associées
+// $req = "SELECT questions.titre_question, options.option_text 
+//         FROM questions 
+//         LEFT JOIN options ON questions.id_question = options.id_question
+//         WHERE questions.id_question = $id_question";
 
-$result = mysqli_query($cnx, $req);
+// $result = mysqli_query($cnx, $req);
 
-if ($result) {
-    if (mysqli_num_rows($result) > 0) {
-        $question_info = mysqli_fetch_assoc($result);
-        $titre_question = $question_info['titre_question'];
-        echo "<h1>" . htmlspecialchars($titre_question) . "</h1>";
+// if ($result) {
+//     if (mysqli_num_rows($result) > 0) {
+//         $question_info = mysqli_fetch_assoc($result);
+//         $titre_question = $question_info['titre_question'];
+//         echo "<h1>" . htmlspecialchars($titre_question) . "</h1>";
 
-        // Afficher les options
-        echo "<ul>";
-        do {
-            $option_text = $question_info['option_text'];
-            echo "<li>" . htmlspecialchars($option_text) . "</li>";
-        } while ($question_info = mysqli_fetch_assoc($result));
-        echo "</ul>";
-    } else {
-        echo "Aucun résultat trouvé pour l'id_question=$id_question";
-    }
-} else {
-    echo "Erreur lors de l'exécution de la requête : " . mysqli_error($cnx);
-}
+//         // Afficher les options
+//         echo "<ul>";
+//         do {
+//             $option_text = $question_info['option_text'];
+//             echo "<li>" . htmlspecialchars($option_text) . "</li>";
+//         } while ($question_info = mysqli_fetch_assoc($result));
+//         echo "</ul>";
+//     } else {
+//         echo "Aucun résultat trouvé pour l'id_question=$id_question";
+//     }
+// } else {
+//     echo "Erreur lors de l'exécution de la requête : " . mysqli_error($cnx);
+// }
 
-mysqli_close($cnx);
-?>
+// mysqli_close($cnx);
+?> */ -->
 
 
 
@@ -156,3 +156,222 @@ mysqli_close($cnx);
 <label for="a4"><input type="radio" name="variable" value="0" id="a4"/> Langage de balisage</label> -->
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+<?php
+
+
+require "conexion.php";
+
+// $id_question = 3; // Remplacez par l'id de la question que vous souhaitez récupérer
+
+// // Sélectionnez le titre de la question
+// $req_question = "SELECT titre_question FROM questions WHERE id_question = $id_question";
+// $result_question = mysqli_query($cnx, $req_question);
+
+// if ($result_question) {
+//     $question_info = mysqli_fetch_assoc($result_question);
+
+//     if ($question_info) {
+//         $titre_question = $question_info['titre_question'];
+//         echo "<h1>" . htmlspecialchars($titre_question) . "</h1>";
+//     } else {
+//         echo "Aucun résultat trouvé pour l'id_question=$id_question";
+//     }
+// } else {
+//     echo "Erreur lors de l'exécution de la requête pour la question : " . mysqli_error($cnx);
+// }
+
+// //Sélectionnez les options de la question
+// $req_options = "SELECT text_option FROM options WHERE id_question = $id_question";
+// $result_options = mysqli_query($cnx, $req_options);
+
+// if ($result_options) {
+//     if (mysqli_num_rows($result_options) > 0) {
+//         echo "<div>";
+
+//         while ($option_info = mysqli_fetch_assoc($result_options)) {
+//             $text_option = $option_info['text_option'];
+//             echo "<input type='radio' name='options' value='" . htmlspecialchars($text_option) . "'>";
+//             echo htmlspecialchars($text_option) ."<br>";
+//         }
+
+//         echo "</div>";
+//     } else {
+//         echo "Aucune option trouvée pour l'id_question=$id_question";
+//     }
+// } else {
+//     echo "Erreur lors de l'exécution de la requête pour les options : " . mysqli_error($cnx);
+// }
+
+//  mysqli_close($cnx);
+// ?>
+<!-- // <button type="button">next</button> -->
+
+
+
+
+
+<?php
+// ... (connexion à la base de données et récupération des données)
+
+// $id_question = isset($_GET['id_question']) ? intval($_GET['id_question']) : 1;
+
+// // Récupérer le titre de la question
+// $req_question = "SELECT id_question, titre_question FROM questions WHERE id_question = $id_question";
+// $result_question = mysqli_query($cnx, $req_question);
+
+// if ($result_question) {
+//     $question_info = mysqli_fetch_assoc($result_question);
+
+//     if ($question_info) {
+//         $titre_question = $question_info['titre_question'];
+//         $id_question = $question_info['id_question'];
+//         echo "<h1>" . htmlspecialchars($titre_question) . "</h1>";
+
+//         // Récupérer les options de la question
+//         $req_options = "SELECT text_option FROM options WHERE id_question = $id_question";
+//         $result_options = mysqli_query($cnx, $req_options);
+
+//         if ($result_options) {
+//             if (mysqli_num_rows($result_options) > 0) {
+//                 echo "<form id='optionsForm'>";
+        
+//                 while ($option_info = mysqli_fetch_assoc($result_options)) {
+//                     $text_option = $option_info['text_option'];
+//                     echo "<input type='radio' name='selected_option' value='" . htmlspecialchars($text_option) . "'>";
+//                     echo htmlspecialchars($text_option) ."<br>";
+//                 }
+        
+//                 echo "<input type='hidden' name='id_question' value='$id_question'>";
+//                 echo "<button type='button' onclick='loadNextQuestion()'>Next</button>";
+//                 echo "</form>";
+//             } else {
+//                 echo "Aucune option trouvée pour l'id_question=$id_question";
+//             }
+//         } else {
+//             echo "Erreur lors de l'exécution de la requête pour les options : " . mysqli_error($cnx);
+//         }
+//     } else {
+//         echo "Aucun résultat trouvé pour l'id_question=$id_question";
+//     }
+// } else {
+//     echo "Erreur lors de l'exécution de la requête pour la question : " . mysqli_error($cnx);
+// }
+
+// mysqli_close($cnx);
+// ?>
+// <script>
+// function loadNextQuestion() {
+//     var form = document.getElementById('optionsForm');
+//     var selectedOption = form.querySelector('input[name="selected_option"]:checked');
+
+//     if (selectedOption) {
+//         // Envoyer une requête asynchrone pour récupérer la prochaine question
+//         var xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = function() {
+//             if (xhr.readyState == 4 && xhr.status == 200) {
+//                 // Mettre à jour le contenu de la page avec la nouvelle question
+//                 var response = JSON.parse(xhr.responseText);
+//                 document.getElementById('questionContainer').innerHTML = response.question_html;
+
+//                 // Réinitialiser les boutons radio
+//                 var radioButtons = form.querySelectorAll('input[name="selected_option"]');
+//                 radioButtons.forEach(function(button) {
+//                     button.checked = false;
+//                 });
+//             }
+//         };
+
+//         // Envoyer une requête GET au fichier PHP pour obtenir la prochaine question
+//         var nextQuestionId = <?php echo $id_question + 1; ?>;
+//         xhr.open('GET', 'question.php?id_question=' + nextQuestionId, true);
+//         xhr.send();
+//     } else {
+//         alert("Veuillez sélectionner une option avant de passer à la question suivante.");
+//     }
+// }
+// </script>
+<?php
+// ... (connexion à la base de données et récupération des données)
+
+$id_question = isset($_GET['id_question']) ? intval($_GET['id_question']) : 1;
+
+// Récupérer le titre de la question
+$req_question = "SELECT id_question, titre_question FROM questions WHERE id_question = $id_question";
+$result_question = mysqli_query($cnx, $req_question);
+
+if ($result_question) {
+    $question_info = mysqli_fetch_assoc($result_question);
+
+    if ($question_info) {
+        $titre_question = $question_info['titre_question'];
+        $id_question = $question_info['id_question'];
+        echo "<h1>" . htmlspecialchars($titre_question) . "</h1>";
+
+        // Récupérer les options de la question
+        $req_options = "SELECT text_option FROM options WHERE id_question = $id_question";
+        $result_options = mysqli_query($cnx, $req_options);
+
+        if ($result_options) {
+            if (mysqli_num_rows($result_options) > 0) {
+                echo "<form id='optionsForm' action='question.php' method='get'>";
+        
+                while ($option_info = mysqli_fetch_assoc($result_options)) {
+                    $text_option = $option_info['text_option'];
+                    echo "<input type='radio' name='selected_option' value='" . htmlspecialchars($text_option) . "'>";
+                    echo htmlspecialchars($text_option) ."<br>";
+                }
+        
+                echo "<input type='hidden' name='id_question' value='$id_question'>";
+                echo "<button type='button' onclick='loadNextQuestion()'>Next</button>";
+                echo "</form>";
+            } else {
+                echo "Aucune option trouvée pour l'id_question=$id_question";
+            }
+        } else {
+            echo "Erreur lors de l'exécution de la requête pour les options : " . mysqli_error($cnx);
+        }
+    } else {
+        echo "Aucun résultat trouvé pour l'id_question=$id_question";
+    }
+} else {
+    echo "Erreur lors de l'exécution de la requête pour la question : " . mysqli_error($cnx);
+}
+
+mysqli_close($cnx);
+?>
+
+<script>
+function loadNextQuestion() {
+    var form = document.getElementById('optionsForm');
+    var selectedOption = form.querySelector('input[name="selected_option"]:checked');
+
+    if (selectedOption) {
+        // Envoyer une requête asynchrone pour récupérer la prochaine question
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Actualiser la page avec la nouvelle question
+                document.open();
+                document.write(xhr.responseText);
+                document.close();
+            }
+        };
+
+        // Envoyer une requête GET au fichier PHP pour obtenir la prochaine question
+        var nextQuestionId = <?php echo $id_question + 1; ?>;
+        xhr.open('GET', 'question.php?id_question=' + nextQuestionId, true);
+        xhr.send();
+    } else {
+        alert("Veuillez sélectionner une option avant de passer à la question suivante.");
+ }
+}
+</script>
